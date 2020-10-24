0b0253064fb6fc7846206991f10292e1a6595c2a
Sammy Liu <liuzhihao1991@gmail.com>
2020-10-23 21:19:33 -0400

    setup cosmos project

---
+++ x/cosmostictactoe/client/cli/tx.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/sammy1991106/cosmos-tic-tac-toe/x/cosmostictactoe/types"
+)
+
+// GetTxCmd returns the transaction commands for this module
+func GetTxCmd(cdc *codec.Codec) *cobra.Command {
+	cosmostictactoeTxCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cosmostictactoeTxCmd.AddCommand(flags.PostCommands(
+	// this line is used by starport scaffolding # 1
+	)...)
+
+	return cosmostictactoeTxCmd
+}
---
+++ x/cosmostictactoe/genesis.go
@@ -0,0 +1,22 @@
+package cosmostictactoe
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/sammy1991106/cosmos-tic-tac-toe/x/cosmostictactoe/keeper"
+	"github.com/sammy1991106/cosmos-tic-tac-toe/x/cosmostictactoe/types"
+	// abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// InitGenesis initialize default parameters
+// and the keeper's address to pubkey map
+func InitGenesis(ctx sdk.Context, k keeper.Keeper /* TODO: Define what keepers the module needs */, data types.GenesisState) {
+	// TODO: Define logic for when you would like to initalize a new genesis
+}
+
+// ExportGenesis writes the current store values
+// to a genesis file, which can be imported again
+// with InitGenesis
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) (data types.GenesisState) {
+	// TODO: Define logic for exporting state
+	return types.NewGenesisState()
+}
